986129022801ed5bdc0a9c50f2353c84df5fa43d
Sapt75 <109506771+Sapt75@users.noreply.github.com>
2023-01-03 12:20:26 +0530

    Final Commit

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home(blogs) {
               <div className="container relative z-30 pt-20 pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48">
                 <div className="flex flex-col items-center justify-center lg:flex-row">
                   <div className="rounded-full border-8 border-primary shadow-xl">
-                    <Image width={80} height={80} src="/assets/img/blog-author.jpg" className="h-48 rounded-full sm:h-56" alt="author" />
+                    <Image width={240} height={240} src="/assets/img/blog-author.jpg" className="h-48 rounded-full sm:h-56" alt="author" />
                   </div>
                   <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
                     <h1 className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
